6ad3472d62cd6d1225764b09e56f7fd92be8d2de
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-19 20:00:40 -0300

    feat: Adicionado o header ao app.tsx para ser renderizado na tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import Header from "./components/Header";
 
 function App() {
-    return {
-        
-    }
+    return (
+        <BrowserRouter>
+            <Header />
+        </BrowserRouter>
+    )
 }
 export default App
