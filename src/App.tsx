a8ed649e3c7f16c126a4c0e4acac4335fcc75ab8
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-20 16:10:35 -0300

    feat: criado todas as rotas e importações para o app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
 import Header from "./components/Header";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cafe from "./pages/Cafe";
+import Coworking from "./pages/Coworking";
+import Contact from "./pages/Contact";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Footer/>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cafe" element={<Cafe />} />
+        <Route path="/coworking" element={<Coworking />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
-export default App
+
+export default App;
