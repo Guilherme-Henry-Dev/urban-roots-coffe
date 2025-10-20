3ec3a4b05dd2b05e3261a06390825fd4057a4e15
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-20 15:51:51 -0300

    feat: criado o footer da página e adicionado ao app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Header from "./components/Header";
+import Footer from "./components/Footer"
 
 function App() {
     return (
         <BrowserRouter>
             <Header />
+            <Footer/>
         </BrowserRouter>
     )
 }
