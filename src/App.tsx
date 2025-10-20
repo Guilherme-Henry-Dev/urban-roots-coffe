e59de2f8b538d593e3a554ab9dcc26090f2cdcb5
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-10-20 20:43:48 -0300

    feat: corrigido imports para maneira correta

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PageTransition from "./components/PageTransition";
